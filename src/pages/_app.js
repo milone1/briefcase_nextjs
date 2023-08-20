41e17d7bf9f00880a0b1a410ccb77bd233cd6e64
Erick Millan <erickmilan1002@gmail.com>
2023-08-19 21:25:15 -0500

    add images meta datas of opengrap

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -13,19 +13,23 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
-        <title>Briefcase Erick Millan Marin - Software Developer</title>
-        <meta name="description" content="Welcome to my personal portfolio! I'm Erick Millan Marin, a skilled software developer passionate about creating impactful applications." />
+        <title>Erick Millan Marin - Software Developer Portfolio</title>
+        <meta name="description" content="Explore the projects and skills of Erick Millan Marin, a dedicated software developer focused on delivering efficient and user-friendly applications." />
+
+        {/* Open Graph Meta Tags */}
+        <meta property="og:url" content="https://briefcase-erickmilan.vercel.app" />
+        <meta property="og:type" content="website" />
         <meta property="og:title" content="Erick Millan Marin - Software Developer Portfolio" />
         <meta property="og:description" content="Explore the projects and skills of Erick Millan Marin, a dedicated software developer focused on delivering efficient and user-friendly applications." />
-        <meta property="og:image" content={`${process.env.NEXT_PUBLIC_BASE_URL}/public/screen.webp`} />
-        {/* <meta property="og:image" content="/public/screen.webp" /> */}
-        <meta property="og:url" content="https://briefcase-erickmilan.vercel.app" />
+        <meta property="og:image" content="https://briefcase-erickmilan.vercel.app/public/screen.webp" />
+
+        {/* Twitter Card Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@erickdev" />
-        <meta name="twitter:creator" content="@erickdev" />
+        <meta name="twitter:domain" content="briefcase-erickmilan.vercel.app" />
+        <meta name="twitter:url" content="https://briefcase-erickmilan.vercel.app" />
         <meta name="twitter:title" content="Erick Millan Marin - Software Developer Portfolio" />
-        <meta name="twitter:description" content="Discover the software development journey of Erick Millan Marin through a collection of innovative projects and skills." />
-        <meta name="twitter:image" content="/public/screen.webp" />
+        <meta name="twitter:description" content="Explore the projects and skills of Erick Millan Marin, a dedicated software developer focused on delivering efficient and user-friendly applications." />
+        <meta name="twitter:image" content="https://briefcase-erickmilan.vercel.app/public/screen.webp" />
       </Head>
       <AnimatePresence mode='wait'>
         <motion.div key={router.route} className='h-full'>
