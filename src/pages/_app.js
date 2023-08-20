71543c0817cefb067063302a4aca2a9a72a9916a
Erick Millan <erickmilan1002@gmail.com>
2023-08-19 21:15:37 -0500

    add metatags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -17,14 +17,14 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="Welcome to my personal portfolio! I'm Erick Millan Marin, a skilled software developer passionate about creating impactful applications." />
         <meta property="og:title" content="Erick Millan Marin - Software Developer Portfolio" />
         <meta property="og:description" content="Explore the projects and skills of Erick Millan Marin, a dedicated software developer focused on delivering efficient and user-friendly applications." />
-        <meta property="og:image" content="/images/portfolio_preview.png" />
-        <meta property="og:url" content="https://www.erickmillanmarin.com" />
+        <meta property="og:image" content="/public/screen.webp" />
+        <meta property="og:url" content="https://briefcase-erickmilan.vercel.app" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@erickdev" />
         <meta name="twitter:creator" content="@erickdev" />
         <meta name="twitter:title" content="Erick Millan Marin - Software Developer Portfolio" />
         <meta name="twitter:description" content="Discover the software development journey of Erick Millan Marin through a collection of innovative projects and skills." />
-        <meta name="twitter:image" content="/images/portfolio_preview.png" />
+        <meta name="twitter:image" content="/public/screen.webp" />
       </Head>
       <AnimatePresence mode='wait'>
         <motion.div key={router.route} className='h-full'>
